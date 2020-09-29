5dc800454f521559923b532f762c3e98af0ca44b
edgardodth <edgardodth@gmail.com>
2020-09-29 11:33:26 -0500

    implementar area del cuadrado

--- main.go
+++ main.go
@@ -2,6 +2,11 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+    var lado int32
+    fmt.Scanf("%d", &lado)
+
+    area := lado * lado
+
+    fmt.Println(area)
 }
